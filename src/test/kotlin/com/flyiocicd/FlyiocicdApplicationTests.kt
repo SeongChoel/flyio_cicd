99ffd64c0bb1490e09a0182f6427756260942ce6
SeongChoel <tldtlddl1234@gmail.com>
2025-09-12 11:26:06 +0900

    Test, HomeController

--- src/test/kotlin/com/flyiocicd/FlyiocicdApplicationTests.kt
+++ src/test/kotlin/com/flyiocicd/FlyiocicdApplicationTests.kt
@@ -1,13 +1,40 @@
 package com.flyiocicd
 
+import com.flyiocicd.domain.home.home.controller.HomeController
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class FlyiocicdApplicationTests {
 
+    @Autowired
+    private lateinit var mvc: MockMvc
+
+
     @Test
-    fun contextLoads() {
+    @DisplayName("메인")
+    fun t1() {
+        val resultActions = mvc
+            .perform(
+                get("/")
+            )
+            .andDo { MockMvcResultHandlers.print() }
+
+        resultActions.andExpect {
+            MockMvcResultMatchers.handler().handlerType(HomeController::class.java)
+            MockMvcResultMatchers.handler().methodName("main")
+            MockMvcResultMatchers.status().isOk
+        }
     }
 
 }

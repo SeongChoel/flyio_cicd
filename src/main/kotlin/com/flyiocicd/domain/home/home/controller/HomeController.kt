79412934e481fb69568145bd730aa5a6f4af80ea
SeongChoel <tldtlddl1234@gmail.com>
2025-09-12 14:19:38 +0900

    logger

--- src/main/kotlin/com/flyiocicd/domain/home/home/controller/HomeController.kt
+++ src/main/kotlin/com/flyiocicd/domain/home/home/controller/HomeController.kt
@@ -1,5 +1,6 @@
 package com.flyiocicd.domain.home.home.controller
 
+import com.flyiocicd.global.extensions.logger
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -8,11 +9,17 @@ import java.net.InetAddress
 @Controller
 class HomeController {
 
+    private val logger = logger()
+
     @GetMapping
     @ResponseBody
     fun main(): String {
         val localhost = InetAddress.getLocalHost()
         val hostname = localhost.hostName
+
+        println(hostname)
+        logger.info("hostname: $hostname")
+        logger.debug("hostname: $hostname")
 
         return "홈 ,hostname: $hostname"
     }

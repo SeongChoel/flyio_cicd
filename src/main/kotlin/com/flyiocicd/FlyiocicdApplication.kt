a0669a3d47e93ce6abb91a2a81dc71524815dd42
SeongChoel <tldtlddl1234@gmail.com>
2025-09-12 11:11:59 +0900

    init

---
+++ src/main/kotlin/com/flyiocicd/FlyiocicdApplication.kt
@@ -0,0 +1,11 @@
+package com.flyiocicd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlyiocicdApplication
+
+fun main(args: Array<String>) {
+    runApplication<FlyiocicdApplication>(*args)
+}

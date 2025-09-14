f519bc2c7e7bddc264517e18e653c7ed7c692d67
SeongChoel <tldtlddl1234@gmail.com>
2025-09-14 15:29:00 +0900

    yml설정 test,prod,dev

---
+++ src/main/kotlin/com/flyiocicd/domain/home/home/controller/post/post/entity/Post.kt
@@ -0,0 +1,16 @@
+package com.flyiocicd.domain.home.home.controller.post.post.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType.IDENTITY
+import jakarta.persistence.Id
+
+
+@Entity
+class Post(
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    var id: Long? = null,
+    val title: String,
+    var content: String
+)
